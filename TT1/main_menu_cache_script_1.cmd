7c04e91625f41b73a08f5886f2b01f3ffebb126e
InsertStringNameHere <107611247+InsertStringNameHere@users.noreply.github.com>
2022-06-25 15:48:30 +1000

    Rename Roster Animation Tags
    I'm, very unsure about how this has been named. The tag structure corresponds with a modified version of the "ui\halox\common\roster\animations\partyup_flash" tag, from Halo 3. Hopefully this issue will become redundant if I can get Halo 3's roster UI working again.

--- TT1/main_menu_cache_script_1.cmd
+++ TT1/main_menu_cache_script_1.cmd
@@ -475,7 +475,7 @@ nametag 0x09F1 ui\halox\common\roster\ringspeak_ui
 nametag 0x09F3 ui\halox\common\roster\animations\outer_ring
 nametag 0x09F4 ui\halox\common\roster\animations\middle_ring
 nametag 0x09F5 ui\halox\common\roster\rank_tray_ui
-nametag 0x09F6 ui\halox\common\roster\animations\mp_list_bitmap
+nametag 0x09F6 ui\halox\common\roster\animations\partyup_flash
 nametag 0x09F7 ui\halox\common\roster\roster_flashy_ui
 nametag 0x09F8 ui\halox\common\roster\blankcheck_ui
 nametag 0x09F9 ui\halox\common\roster\animations\playername_lose_focus
@@ -502,8 +502,8 @@ nametag 0x0A0D ui\halox\common\roster\animations\rating_lose_focus
 nametag 0x0A0E ui\halox\common\roster\animations\rating_scale_down
 nametag 0x0A0F ui\halox\common\roster\animations\rating_rec_focus
 nametag 0x0A10 ui\halox\common\roster\animations\rating_scale_up
-nametag 0x0A12 ui\halox\global_animations\animation_definitions\hidden
-nametag 0x0A13 ui\halox\global_animations\animations\hidden
+nametag 0x0A12 ui\halox\common\roster\animations\partyup_flash
+nametag 0x0A13 ui\halox\common\roster\animations\partyup_flash
 nametag 0x0A16 ui\halox\pregame_lobby\animations\precaching\precache_test
 nametag 0x0A17 ui\halox\common\common_bitmaps\black_10
 nametag 0x0A18 ui\halox\pregame_lobby\animations\precaching\precache_wipe
@@ -1912,8 +1912,6 @@ nametag 0x573D levels\multi\guardian\bitmap\gfxt
 nametag 0x573E levels\multi\s3d_avalanche\bitmap\gfxt
 deletetag 0x028A
 deletetag 0x028C
-deletetag 0x0A12
-deletetag 0x0A13
 deletetag 0x0AF9
 deletetag 0x12B9
 deletetag 0x12BC
@@ -1983,11 +1981,6 @@ savetagchanges
 exit
 edittag 0x0B1B
 setfield gameenginesettings[0].options[4].valuepairs 0x0BAB
-savetagchanges
-exit
-edittag 0x09F6
-setfield unknown 0x086C
-setfield unknown2 0x086C
 savetagchanges
 exit
 edittag 0x028E
