674480f36d88b149aba3da2ff3b6930591f9b7ef
InsertStringNameHere <107611247+InsertStringNameHere@users.noreply.github.com>
2022-06-25 20:03:39 +1000

    Add Weapon and Chud Porting Commands

--- TT2/main_menu_cache_script_2.cmd
+++ TT2/main_menu_cache_script_2.cmd
@@ -4,6 +4,106 @@ exit
 opencachefile "../H3Maps/100_citadel.map"
 porttag ui\single_player.wgtz
 exit
+opencachefile "../H3MythicMaps/sandbox.map"
+porttag ui\chud\animations\backpack_test.chud_animation_definition
+porttag ui\chud\animations\backpack_unready.chud_animation_definition
+porttag ui\chud\animations\backpack_warning_flash.chud_animation_definition
+porttag ui\chud\animations\beam_rifle_meter_left.chud_animation_definition
+porttag ui\chud\animations\beam_rifle_meter_right.chud_animation_definition
+porttag ui\chud\animations\carbine_crosshair.chud_animation_definition
+porttag ui\chud\animations\carbine_crosshair2.chud_animation_definition
+porttag ui\chud\animations\center_dimming.chud_animation_definition
+porttag ui\chud\animations\center_hack.chud_animation_definition
+porttag ui\chud\animations\clockwise.chud_animation_definition
+porttag ui\chud\animations\counterclockwise.chud_animation_definition
+porttag ui\chud\animations\ctf_scoreboard.chud_animation_definition
+porttag ui\chud\animations\dimming.chud_animation_definition
+porttag ui\chud\animations\equipment_kablam.chud_animation_definition
+porttag ui\chud\animations\fade_in.chud_animation_definition
+porttag ui\chud\animations\fade_in_slow.chud_animation_definition
+porttag ui\chud\animations\flash_test.chud_animation_definition
+porttag ui\chud\animations\init_wipe_horizontal.chud_animation_definition
+porttag ui\chud\animations\init_wipe_vertical.chud_animation_definition
+porttag ui\chud\animations\laser_charge.chud_animation_definition
+porttag ui\chud\animations\laser_unzoom_level.chud_animation_definition
+porttag ui\chud\animations\laser_zoom_fade_level.chud_animation_definition
+porttag ui\chud\animations\laser_zoom_level.chud_animation_definition
+porttag ui\chud\animations\lock_flash.chud_animation_definition
+porttag ui\chud\animations\motion_tracker_ping.chud_animation_definition
+porttag ui\chud\animations\pitch_slide.chud_animation_definition
+porttag ui\chud\animations\shield_dimming.chud_animation_definition
+porttag ui\chud\animations\shields_out.chud_animation_definition
+porttag ui\chud\animations\sniper_slide.chud_animation_definition
+porttag ui\chud\animations\vip_aura_center.chud_animation_definition
+porttag ui\chud\animations\vip_aura_pulsate.chud_animation_definition
+porttag ui\chud\animations\vip_notify.chud_animation_definition
+porttag ui\chud\animations\vip_notify_text.chud_animation_definition
+porttag ui\chud\animations\vox1.chud_animation_definition
+porttag ui\chud\animations\vox2.chud_animation_definition
+porttag ui\chud\animations\yaw_slide.chud_animation_definition
+porttag ui\chud\animations\zoom_scope.chud_animation_definition
+exit
+opencachefile "../H3MythicMaps/sandbox.map"
+porttag ui\chud\assault_rifle.chud_definition
+porttag ui\chud\battle_rifle.chud_definition
+porttag ui\chud\beam_rifle.chud_definition
+porttag ui\chud\brute_shot.chud_definition
+porttag ui\chud\carbine.chud_definition
+porttag ui\chud\e_bubbleshield.chud_definition
+porttag ui\chud\e_flare.chud_definition
+porttag ui\chud\e_gravlift.chud_definition
+porttag ui\chud\e_instacover.chud_definition
+porttag ui\chud\e_jammer.chud_definition
+porttag ui\chud\e_powerdrain.chud_definition
+porttag ui\chud\e_regen.chud_definition
+porttag ui\chud\e_tripmine.chud_definition
+porttag ui\chud\elite.chud_definition
+porttag ui\chud\excavator.chud_definition
+porttag ui\chud\flamethrower.chud_definition
+porttag ui\chud\fuel_rod_cannon.chud_definition
+porttag ui\chud\golf_club.chud_definition
+porttag ui\chud\hammer.chud_definition
+porttag ui\chud\machinegun_turret.chud_definition
+porttag ui\chud\magnum.chud_definition
+porttag ui\chud\missile.chud_definition
+porttag ui\chud\monitor.chud_definition
+porttag ui\chud\needler.chud_definition
+porttag ui\chud\plasma_pistol.chud_definition
+porttag ui\chud\plasma_rifle.chud_definition
+porttag ui\chud\plasma_turret.chud_definition
+porttag ui\chud\rocket.chud_definition
+porttag ui\chud\sentinel_beam.chud_definition
+porttag ui\chud\shotgun.chud_definition
+porttag ui\chud\smg.chud_definition
+porttag ui\chud\sniper_rifle.chud_definition
+porttag ui\chud\spartan.chud_definition
+porttag ui\chud\spartan_laser.chud_definition
+porttag ui\chud\spike_rifle.chud_definition
+porttag ui\chud\sword.chud_definition
+porttag ui\chud\t_machinegun.chud_definition
+porttag ui\chud\t_plasmaturret.chud_definition
+porttag ui\chud\unarmed.chud_definition
+porttag ui\chud\v_banshee.chud_definition
+porttag ui\chud\v_chaingun.chud_definition
+porttag ui\chud\v_chopper.chud_definition
+porttag ui\chud\v_driver.chud_definition
+porttag ui\chud\v_gaussturret.chud_definition
+porttag ui\chud\v_ghost.chud_definition
+porttag ui\chud\v_grinderdriver.chud_definition
+porttag ui\chud\v_grinderturret.chud_definition
+porttag ui\chud\v_hornet.chud_definition
+porttag ui\chud\v_mongoose.chud_definition
+porttag ui\chud\v_scorpion.chud_definition
+porttag ui\chud\v_scorpionturret.chud_definition
+porttag ui\chud\v_wraith.chud_definition
+porttag ui\chud\v_wraith_anti_air.chud_definition
+porttag ui\chud\v_wraithturret.chud_definition
+exit
+opencachefile "../H3Maps/100_citadel.map"
+porttag ui\chud\e_autoturret.chud_definition
+porttag ui\chud\e_invincibility.chud_definition
+porttag ui\chud\e_invisibility.chud_definition
+exit
 opencachefile "../H3Maps/100_citadel.map"
 porttag objects\characters\dervish\fp\fp.mode
 porttag objects\characters\dervish\fp_body\fp_body.mode
@@ -35,9 +135,48 @@ porttag objects\weapons\grenade\frag_grenade\frag_grenade.proj
 porttag objects\weapons\grenade\frag_grenade\frag_grenade.eqip
 porttag objects\weapons\grenade\plasma_grenade\plasma_grenade.proj
 porttag objects\weapons\grenade\plasma_grenade\plasma_grenade.eqip
+porttag objects\weapons\melee\energy_blade\energy_blade.weap
+porttag objects\weapons\melee\energy_blade\energy_blade_useless.weap
+porttag objects\weapons\melee\gravity_hammer\gravity_hammer.weap
 porttag objects\weapons\multiplayer\assault_bomb\assault_bomb.weap
 porttag objects\weapons\multiplayer\ball\ball.weap
 porttag objects\weapons\multiplayer\flag\flag.weap
+porttag objects\weapons\pistol\excavator\excavator.weap
+porttag objects\weapons\pistol\magnum\magnum.weap
+porttag objects\weapons\pistol\needler\needler.weap
+porttag objects\weapons\pistol\plasma_pistol\plasma_pistol.weap
+porttag objects\weapons\rifle\assault_rifle\assault_rifle.weap
+porttag objects\weapons\rifle\battle_rifle\battle_rifle.weap
+porttag objects\weapons\rifle\beam_rifle\beam_rifle.weap
+porttag objects\weapons\rifle\covenant_carbine\covenant_carbine.weap
+porttag objects\weapons\rifle\plasma_rifle\plasma_rifle.weap
+porttag objects\weapons\rifle\shotgun\shotgun.weap
+porttag objects\weapons\rifle\smg\smg.weap
+porttag objects\weapons\rifle\sniper_rifle\sniper_rifle.weap
+porttag objects\weapons\rifle\spike_rifle\spike_rifle.weap
+porttag objects\weapons\support_high\rocket_launcher\rocket_launcher.weap
+porttag objects\weapons\support_high\spartan_laser\spartan_laser.weap
+porttag objects\weapons\support_low\brute_shot\brute_shot.weap
+porttag objects\weapons\turret\flamethrower\flamethrower.weap
+porttag objects\weapons\turret\missile_pod\missile_pod.weap
+exit
+opencachefile "../H3Maps/100_citadel.map"
+porttag objects\equipment\instantcover_equipment\instantcover_equipment.eqip
+porttag objects\equipment\invincibility_equipment\invincibility_equipment.eqip
+porttag objects\equipment\invisibility_equipment\invisibility_equipment.eqip
+exit
+opencachefile "../H3MythicMaps/sandbox.map"
+porttag objects\equipment\bubbleshield_equipment\bubbleshield_equipment.eqip
+porttag objects\equipment\gravlift_equipment\gravlift_equipment.eqip
+porttag objects\equipment\instantcover_equipment\instantcover_equipment_mp.eqip
+porttag objects\equipment\jammer_equipment\jammer_equipment.eqip
+porttag objects\equipment\powerdrain_equipment\powerdrain_equipment.eqip
+porttag objects\equipment\regenerator_equipment\regenerator_equipment.eqip
+porttag objects\equipment\superflare_equipment\superflare_equipment.eqip
+porttag objects\equipment\tripmine_equipment\tripmine_equipment.eqip
+exit
+opencachefile "../H3Maps/120_halo.map"
+porttag objects\equipment\autoturret_equipment\autoturret_equipment.eqip
 exit
 edittag globals\globals.globals
 setfield grenades[0].equipment objects\weapons\grenade\frag_grenade\frag_grenade.eqip
@@ -78,6 +217,43 @@ setfield playerrepresentation[5].thirdpersonunit objects\characters\monitor\moni
 savetagchanges
 exit
 edittag multiplayer\multiplayer_globals.multiplayer_globals
+setfield universal[0].equipment[0].object null
+setfield universal[0].equipment[1].object objects\equipment\jammer_equipment\jammer_equipment.eqip
+setfield universal[0].equipment[2].object objects\equipment\powerdrain_equipment\powerdrain_equipment.eqip
+setfield universal[0].equipment[3].object objects\equipment\bubbleshield_equipment\bubbleshield_equipment.eqip
+setfield universal[0].equipment[4].object objects\equipment\superflare_equipment\superflare_equipment.eqip
+setfield universal[0].equipment[5].object objects\equipment\regenerator_equipment\regenerator_equipment.eqip
+setfield universal[0].equipment[6].object objects\equipment\tripmine_equipment\tripmine_equipment.eqip
+setfield universal[0].equipment[7].object objects\equipment\instantcover_equipment\instantcover_equipment_mp.eqip
+setfield universal[0].equipment[8].object objects\equipment\invincibility_equipment\invincibility_equipment.eqip
+setfield universal[0].equipment[9].object objects\equipment\gravlift_equipment\gravlift_equipment.eqip
+setfield universal[0].equipment[10].object objects\equipment\autoturret_equipment\autoturret_equipment.eqip
+setfield universal[0].equipment[11].object objects\equipment\invisibility_equipment\invisibility_equipment.eqip
+setfield universal[0].equipment[12].object objects\equipment\instantcover_equipment\instantcover_equipment.eqip
+setfield universal[0].gamevariantweapons[0].weapon objects\weapons\rifle\battle_rifle\battle_rifle.weap
+setfield universal[0].gamevariantweapons[1].weapon objects\weapons\rifle\assault_rifle\assault_rifle.weap
+setfield universal[0].gamevariantweapons[2].weapon objects\weapons\pistol\plasma_pistol\plasma_pistol.weap
+setfield universal[0].gamevariantweapons[3].weapon objects\weapons\rifle\spike_rifle\spike_rifle.weap
+setfield universal[0].gamevariantweapons[4].weapon objects\weapons\rifle\smg\smg.weap
+setfield universal[0].gamevariantweapons[5].weapon objects\weapons\rifle\covenant_carbine\covenant_carbine.weap
+setfield universal[0].gamevariantweapons[6].weapon objects\weapons\melee\energy_blade\energy_blade.weap
+setfield universal[0].gamevariantweapons[7].weapon objects\weapons\pistol\magnum\magnum.weap
+setfield universal[0].gamevariantweapons[8].weapon objects\weapons\pistol\needler\needler.weap
+setfield universal[0].gamevariantweapons[9].weapon objects\weapons\rifle\plasma_rifle\plasma_rifle.weap
+setfield universal[0].gamevariantweapons[10].weapon objects\weapons\support_high\rocket_launcher\rocket_launcher.weap
+setfield universal[0].gamevariantweapons[11].weapon objects\weapons\rifle\shotgun\shotgun.weap
+setfield universal[0].gamevariantweapons[12].weapon objects\weapons\rifle\sniper_rifle\sniper_rifle.weap
+setfield universal[0].gamevariantweapons[13].weapon objects\weapons\support_low\brute_shot\brute_shot.weap
+setfield universal[0].gamevariantweapons[14].weapon objects\weapons\melee\energy_blade\energy_blade_useless.weap
+setfield universal[0].gamevariantweapons[15].weapon objects\weapons\rifle\beam_rifle\beam_rifle.weap
+setfield universal[0].gamevariantweapons[16].weapon objects\weapons\support_high\spartan_laser\spartan_laser.weap
+setfield universal[0].gamevariantweapons[17].weapon null
+setfield universal[0].gamevariantweapons[18].weapon objects\weapons\melee\gravity_hammer\gravity_hammer.weap
+setfield universal[0].gamevariantweapons[19].weapon objects\weapons\pistol\excavator\excavator.weap
+setfield universal[0].gamevariantweapons[20].weapon objects\weapons\turret\flamethrower\flamethrower.weap
+setfield universal[0].gamevariantweapons[21].weapon objects\weapons\turret\missile_pod\missile_pod.weap
+setfield universal[0].gamevariantequipment[0].grenade objects\weapons\grenade\frag_grenade\frag_grenade.eqip
+setfield universal[0].gamevariantequipment[1].grenade objects\weapons\grenade\plasma_grenade\plasma_grenade.eqip
 setfield runtime[0].editorbiped objects\characters\monitor\monitor_editor.bipd
 setfield runtime[0].editorhelperobject objects\ui\editor_gizmo\editor_gizmo.scen
 setfield runtime[0].flag objects\weapons\multiplayer\flag\flag.weap
