833bebf4c21ec1c796c7a84167bd35090e962b67
InsertStringNameHere <107611247+InsertStringNameHere@users.noreply.github.com>
2022-07-29 11:09:59 +1000

    Removed Redundant Commands

--- TT2/main_menu_cache_script_5.cmd
+++ TT2/main_menu_cache_script_5.cmd
@@ -8,38 +8,22 @@ addblockelements hudwidgets[22].placementdata 1
 addblockelements hudwidgets[22].bitmapwidgets 1
 setfield hudwidgets[22].statedata[0].inverseflags notzoomedin
 setfield hudwidgets[22].placementdata[0].anchor equipment
-setfield hudwidgets[22].placementdata[0].origin 0 0
-setfield hudwidgets[22].placementdata[0].offset 0 0
 setfield hudwidgets[22].placementdata[0].scale 1 1
 setfield hudwidgets[22].bitmapwidgets[0].name cons_icon
-setfield hudwidgets[22].bitmapwidgets[0].scriptingclass useparent
 setfield hudwidgets[22].bitmapwidgets[0].sortlayer foreground
 setfield hudwidgets[22].bitmapwidgets[0].runtimewidgetindex 90
 setfield hudwidgets[22].bitmapwidgets[0].flags inputcontrolsconsumable
 setfield hudwidgets[22].bitmapwidgets[0].bitmap ui\chud\bitmaps\equipment_scematics.bitm
-setfield hudwidgets[22].bitmapwidgets[0].bitmapsequenceindex 0
 addblockelements hudwidgets[22].bitmapwidgets[0].statedata 1
 addblockelements hudwidgets[22].bitmapwidgets[0].placementdata 1
 addblockelements hudwidgets[22].bitmapwidgets[0].animationdata 1
 addblockelements hudwidgets[22].bitmapwidgets[0].renderdata 1
 setfield hudwidgets[22].bitmapwidgets[0].statedata[0].consumableflags consumable1available
 setfield hudwidgets[22].bitmapwidgets[0].placementdata[0].anchor parent
-setfield hudwidgets[22].bitmapwidgets[0].placementdata[0].origin 0 0
 setfield hudwidgets[22].bitmapwidgets[0].placementdata[0].offset 27 100
 setfield hudwidgets[22].bitmapwidgets[0].placementdata[0].scale 1 1
 setfield hudwidgets[22].bitmapwidgets[0].animationdata[0].active.animation ui\chud\animations\equipment_kablam.chad
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].shadertype simple
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].blendmodeho alphablend
 setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].externalinput unknownx51
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].rangeinput zero
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localcolora 0 0 0 0 
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localcolorb 0 0 0 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localcolorc 0 0 0 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localcolord 0 0 0 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localscalara 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localscalarb 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localscalarc 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].localscalard 0
 setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].outputcolora highlightforeground
 setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].outputcolorb locala
 setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].outputcolorc locala
@@ -52,10 +36,6 @@ setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].outputscalarc rangeinput
 setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].outputscalard rangeinput
 setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].outputscalare rangeinput
 setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].outputscalarf rangeinput
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].unknown2 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].unknown3 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].unknown4 0
-setfield hudwidgets[22].bitmapwidgets[0].renderdata[0].unknown5 0
 savetagchanges
 exit
 edittag ui\chud\spartan.chud_definition
